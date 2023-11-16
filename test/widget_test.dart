095140c25bab086536e910eff9e110ceffc7b9b9
Ashutosh Kapoor <ashutoshkapoor8965@gmail.com>
2023-11-17 00:09:47 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:auto_infirmary_user/main.dart';
+import 'package:quickcare_user/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
