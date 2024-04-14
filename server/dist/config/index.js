4e8fb042f886036f63d37c0fb1302e91216e2e8a
Deepanshu Rawat <deepanshu.dr10@gmail.com>
2024-04-14 15:52:07 +0530

    pushing latest build

--- server/dist/config/index.js
+++ server/dist/config/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 exports.default = {
     jwtSecret: process.env.JWT_SECRET,
-    bcryptSalt: process.env.BCRYPT_SALT
+    bcryptSalt: process.env.BCRYPT_SALT,
+    port: process.env.PORT || 3000,
 };
 //# sourceMappingURL=index.js.map
--- server/dist/index.js
+++ server/dist/index.js
@@ -11,6 +11,7 @@ const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const compression_1 = __importDefault(require("compression"));
 const routers_1 = __importDefault(require("./routers"));
+const config_1 = __importDefault(require("./config"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
     credentials: true,
@@ -21,8 +22,8 @@ app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
 app.use('/', (0, routers_1.default)());
 // const server = http.createServer(app);
-var port = process.env.PORT || 4207;
-app.listen(port, () => {
-    logger_1.default.info('Server running on http://localhost:' + port);
+const PORT = config_1.default.port;
+app.listen(PORT, () => {
+    logger_1.default.info('Server running on http://localhost:' + PORT);
 });
 //# sourceMappingURL=index.js.map
